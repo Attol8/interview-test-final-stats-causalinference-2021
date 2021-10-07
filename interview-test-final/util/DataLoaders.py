56f36adbbe8f26d2c8d2b11aa84c41c5c316c9d6
Attol8 <jacopoattolini@gmail.com>
2021-10-07 20:47:44 +0200

    implement data loaders

--- interview-test-final/util/DataLoaders.py
+++ interview-test-final/util/DataLoaders.py
@@ -1,6 +1,7 @@
 import logging
 from abc import ABC, abstractmethod
 import os.path
+import pandas as pd
 
 class AbstractDataLoader(ABC):
 
@@ -13,10 +14,10 @@ class AbstractDataLoader(ABC):
 
         if not os.path.isfile(filename):
             logging.error('File does not exist')
-            # TODO: raise exception
+            # TODO: raise exception (comment: I raise the excpetion in the implemented class)
         else:
             logging.info('Found file: ' + filename)
-
+            
 class FileDataLoader(AbstractDataLoader):
 
     # Initialization
@@ -28,10 +29,17 @@ class FileDataLoader(AbstractDataLoader):
     # Load data from file and return data
     def load_data(self):
         # TODO: Check file exists
+        try: 
+            with open(self.filename) as f:
+                logging.info('Found file: ' + self.filename)
+        except FileNotFoundError:
+            raise
+        
 
         # TODO: Load data from file
         logging.info('Loading data using pandas')
+        df = pd.read_csv(self.filename)
 
         # TODO: Return your data object here
-        return 0
+        return df
 
